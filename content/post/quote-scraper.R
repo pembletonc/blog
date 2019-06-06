d6aae1fa427997d22efcd8685550903973d1792b
Corey Pembleton <pembletonc@gmail.com>
2019-06-05 23:07:42 -0400

    first function running

--- content/post/quote-scraper.R
+++ content/post/quote-scraper.R
@@ -1,24 +1,28 @@
-library(rvest)
+library(tidyverse)
+
 #https://www.datacamp.com/community/tutorials/r-web-scraping-rvest
 url <- "https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=simone+de+beauvoir"
+url1 <- "https://www.goodreads.com/quotes/search?utf8=%E2%9C%93&q=rainer+maria+rilke&commit=Search"
 
-get_last_page <- function(html){
+last_page_count <- function(html){
   
-  pages_data <- html %>% 
-    # The '.' indicates the class
-    html_node('.a') %>% 
-    # Extract the raw text as a list
-    html_text()                   
+  path <- read_html(html)
   
-  # The second to last of the buttons is the one
-  pages_data[(length(pages_data)-1)] %>%            
-    # Take the raw string
-    unname() %>%                                     
-    # Convert to number
-    as.numeric()                                     
+  pages_data <- path %>%
+    html_nodes(xpath = "/html/body/div[2]/div[3]/div[1]/div[1]/div[2]/div[22]") %>% 
+    html_nodes("a") %>% 
+    html_text() %>% 
+    str_extract("\\(?[0-9,.]+\\)?") %>%
+    map_int(., parse_integer) %>% enframe(name = NULL) %>% 
+    drop_na() %>% 
+    max(.)
+                                  
 }
 
-first_page <- read_html(url)
-(last_page <- get_last_page(first_page))
 
-html_attr(name = "a")
+(last_page <- last_page_count(url))
+
+#collect all pages from author
+list_of_pages <- str_c(url, '?page=', 1:last_page)
+
+head(list_of_pages)
