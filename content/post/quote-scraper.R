b52ef8f158d80c582dce33de2d8153114de6a925
Corey Pembleton <pembletonc@gmail.com>
2019-06-11 13:45:28 -0400

    Combine all functions into one!

--- content/post/quote-scraper.R
+++ content/post/quote-scraper.R
@@ -4,109 +4,111 @@ library(rvest)
 url <- "https://www.goodreads.com/quotes/search?page=1&q=simone+de+beauvoir&utf8=%E2%9C%93"
 url1 <- "https://www.goodreads.com/quotes/search?page=1&q=rainer+maria+rilke&utf8=%E2%9C%93"
 
-#function to extract the quote text itself
-
-quote_text_function <- function(html){
-  
-  path <- read_html(html)
-  
-  p <- path %>% 
-    html_nodes(xpath=paste(selectr::css_to_xpath(".quoteText"), "/text()")) %>%
-    html_text(trim = TRUE) %>% 
-    enframe(name=NULL)
-  
-  p <- paste(unlist(p$value), collapse = " ") %>% enframe(name=NULL)
-  
-  p %>% 
-    separate_rows(value, sep = "―", convert = TRUE) %>% 
-    naniar::replace_with_na(replace = list(value = c(" "))) %>% 
-    janitor::remove_empty("rows") %>% 
-    rename(Quote = value) %>% 
-    rowid_to_column("ID") %>% 
-    mutate(Quote = str_trim(Quote, side = "both"))
-
-}
-
-#function to extract the ratings on goodreads
-quote_rating_function <- function(html){
-  
-  path <- read_html(html) 
-  
-  path %>% 
-    html_nodes("a.smallText") %>% 
-    html_text(trim = TRUE) %>%
-    enframe(name = NULL) %>% 
-    mutate(value = str_remove_all(value, "likes"),
-           Rating = as.numeric(value)) %>%
-    rowid_to_column("ID") %>% 
-    select(-value)
-  }
-
-#function to extract author name & ensure the quote is the authors (not a commentor)
-author_name_function <- function(html){
-  
-path <- read_html(html)
-  
- titles <-  path %>% 
-    html_nodes("a.authorOrTitle") %>%
-    html_text() %>% 
-    str_trim() %>% 
-    enframe(name = NULL)
-
- path %>% 
-   html_nodes(".authorOrTitle") %>%
-   html_text() %>% 
-   str_trim() %>% 
-   enframe(name = NULL) %>% 
-   anti_join(titles) %>% 
-   rename(Author = value) %>% 
-   mutate(Author = str_remove_all(Author, ",")) %>% 
-   rowid_to_column("ID")
-   
-}
-
-#combining all 
-
-get_data_table <- function(html){
-  
-  quote <-  quote_text_function(html)
-  rating <-  quote_rating_function(html)
-  author <-  author_name_function(html)
-  
-  author %>% 
-    left_join(rating, by = "ID") %>% 
-    left_join(quote, by = "ID")
-  
-}
-
-last_page_count_function <- function(html){
-  
-  path <- read_html(html)
-  
-  pages_data <- path %>%
-    html_nodes(xpath = "/html/body/div[2]/div[3]/div[1]/div[1]/div[2]/div[22]") %>% 
-    html_nodes("a") %>% 
-    html_text() %>% 
-    str_extract("\\(?[0-9,.]+\\)?") %>%
-    map_int(., parse_integer) %>% enframe(name = NULL) %>% 
-    drop_na() %>% 
-    max(.)
-}
-
-page_collect_function <-  function(author){
-  
-  authors_formatted <- tolower(str_replace_all(author, pattern = " ", replacement = "+"))
-  authors_url <- paste("https://www.goodreads.com/quotes/search?page=1&q=", authors_formatted, "&utf8=%E2%9C%93", sep="")
-  last_page_number <- last_page_count_function(authors_url)
-  list_of_pages <- str_replace_all(authors_url, "page=1", paste0("page=", as.character(1:last_page_number)))
-  
-}
-
-
 scrape_multiple <- function(author) {
 
+  #function to extract the quote text itself
+  
+  quote_text_function <- function(html){
+    
+    path <- read_html(html)
+    
+    p <- path %>% 
+      html_nodes(xpath=paste(selectr::css_to_xpath(".quoteText"), "/text()")) %>%
+      html_text(trim = TRUE) %>% 
+      enframe(name=NULL)
+    
+    p <- paste(unlist(p$value), collapse = " ") %>% enframe(name=NULL)
+    
+    p %>% 
+      separate_rows(value, sep = "―", convert = TRUE) %>% 
+      naniar::replace_with_na(replace = list(value = c(" "))) %>% 
+      janitor::remove_empty("rows") %>% 
+      rename(Quote = value) %>% 
+      rowid_to_column("ID") %>% 
+      mutate(Quote = str_trim(Quote, side = "both"))
+    
+  }
+  
+  #function to extract the ratings on goodreads
+  quote_rating_function <- function(html){
+    
+    path <- read_html(html) 
+    
+    path %>% 
+      html_nodes("a.smallText") %>% 
+      html_text(trim = TRUE) %>%
+      enframe(name = NULL) %>% 
+      mutate(value = str_remove_all(value, "likes"),
+             Rating = as.numeric(value)) %>%
+      rowid_to_column("ID") %>% 
+      select(-value)
+  }
+  
+  #function to extract author name & ensure the quote is the authors (not a commentor)
+  author_name_function <- function(html){
+    
+    path <- read_html(html)
+    
+    titles <-  path %>% 
+      html_nodes("a.authorOrTitle") %>%
+      html_text() %>% 
+      str_trim() %>% 
+      enframe(name = NULL)
+    
+    path %>% 
+      html_nodes(".authorOrTitle") %>%
+      html_text() %>% 
+      str_trim() %>% 
+      enframe(name = NULL) %>% 
+      anti_join(titles) %>% 
+      rename(Author = value) %>% 
+      mutate(Author = str_remove_all(Author, ",")) %>% 
+      rowid_to_column("ID")
+    
+  }
+  
+  #function to get the last page count of each author
+  last_page_count_function <- function(html){
+    
+    path <- read_html(html)
+    
+    pages_data <- path %>%
+      html_nodes(xpath = "/html/body/div[2]/div[3]/div[1]/div[1]/div[2]/div[22]") %>% 
+      html_nodes("a") %>% 
+      html_text() %>% 
+      str_extract("\\(?[0-9,.]+\\)?") %>%
+      map_int(., parse_integer) %>% enframe(name = NULL) %>% 
+      drop_na() %>% 
+      max(.)
+  }
+  
+  
+  #combining all 
+  
+  
+  page_collect_function <-  function(author){
+    
+    authors_formatted <- tolower(str_replace_all(author, pattern = " ", replacement = "+"))
+    authors_url <- paste("https://www.goodreads.com/quotes/search?page=1&q=", authors_formatted, "&utf8=%E2%9C%93", sep="")
+    last_page_number <- last_page_count_function(authors_url)
+    list_of_pages <- str_replace_all(authors_url, "page=1", paste0("page=", as.character(1:last_page_number)))
+    
+  }
+  
   list_of_pages <- page_collect_function(author)
 
+  get_data_table <- function(html){
+    
+    quote <-  quote_text_function(html)
+    rating <-  quote_rating_function(html)
+    author_name <-  author_name_function(html)
+    
+    author_name %>% 
+      left_join(rating, by = "ID") %>% 
+      left_join(quote, by = "ID")
+    
+  }
+  
   nested_df <- list_of_pages %>% 
     map(get_data_table) %>% 
     bind_rows() %>% 
@@ -118,7 +120,11 @@ scrape_multiple <- function(author) {
   nested_df %>% 
     mutate(data = map(data, ~mutate(.x, Author_Rank = 1:nrow(.x)))) %>% 
     unnest()
-  }
+}
+
+
+
+
 authors <- list("Simone de Beauvoir")
 
 authors <- list("Simone de Beauvoir", "Rainer Maria Rilke", "Socrates")
