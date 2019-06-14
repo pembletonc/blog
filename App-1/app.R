3f52e00bf50115ad288725a3717688ba3c893cd3
Corey Pembleton <pembletonc@gmail.com>
2019-06-14 16:54:28 -0400

    more notes from tutorial

--- App-1/app.R
+++ App-1/app.R
@@ -5,6 +5,18 @@ ui <- fluidPage(sliderInput(inputId = "num",
                             value = 25, min = 1, max = 100),
                 plotOutput("hist"))
 
+server <- function(input, output){
+  
+  output$hist <-  renderPlot({
+    hist(rnorm(input$num))
+  })
+  
+}
+
+
+shinyApp(ui = ui, server = server)
+
+#notes----
 #inputs with Input_() functions, 
 
 #over a dozen input functions (e.g. text input, date, date range)
@@ -17,21 +29,16 @@ ui <- fluidPage(sliderInput(inputId = "num",
 #same layout for outputs: 
 
 
-server <- function(input, output){
-  
-  output$hist <-  renderPlot({ 
-  hist(rnorm(100))
-  })
-  
-}
+
 #three rules of server functions:
 
-
-
 #1. must be stored in an output$x object
-
 #2. what is saved as output should be with a render*({}) function e.g. renderPrint(), renderPlot(), renderTable()
+#3. access input values with input$ to allow for reactivity
 
-#3. 
 
-shinyApp(ui = ui, server = server)
+#notes - sharing apps
+#need replace local hosting with web server. 
+#can create multiple filed shiny apps with ui.R and server.R with NO app.R
+#simple apps only need app.R, but need to use the correct names of app.R, server.R, or ui.R
+
