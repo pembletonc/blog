41f1e5850d1e5817505b12e5e1fbcec654084a6b
Corey Pembleton <pembletonc@gmail.com>
2019-08-20 12:56:50 -0400

    Completed collection of last 100 tweets from all listed candidates with handles (822)

--- App-1/collect-tweets.R
+++ App-1/collect-tweets.R
@@ -1,28 +1,69 @@
 library(rtweet)
 library(tidyverse)
+library(glue)
 
-handles_orig <- read_csv("./App-1/candidate_twitter_handles.csv", 
-                         locale = locale(encoding = "latin1"),
-                         na = "")
+handles_orig <- read.csv("./App-1/candidate_twitter_handles.csv",
+                         na.strings = "",
+                         colClasses = "character", 
+                         encoding = "UTF-8")
 
 handles_only <- handles_orig %>% 
-  filter(!is.na(twitter_handle)) %>% 
+  filter(!is.na(twitter_handle) & str_length(twitter_handle) <= 15) %>% 
   mutate(twitter_handle = tolower(twitter_handle))
 
-candidate_timelines <- 
-  get_timelines(user = as.character(handles_only$twitter_handle),
-                parse = FALSE,
-                n = 100000,
-                retryonratelimit = TRUE)
+handles_only_char_list <- as.character(handles_only$twitter_handle[1:length(handles_only$twitter_handle)])
 
-candidate_timelines %>% 
-  select() %>% 
+#function from https://stackoverflow.com/questions/42025979/avoid-rate-limit-with-rtweet-get-timeline
+get_timeline_unlimited <- function(users, n){
+  
+  if (length(users) ==0){
+    return(NULL)
+  }
+  
+  rl <- rate_limit(query = "get_timeline")
+  
+  if (length(users) <= rl$remaining){
+    print(glue("Getting data for {length(users)} users"))
+    tweets <- get_timeline(users, n, check = FALSE)  
+  }else{
+    
+    if (rl$remaining > 0){
+      users_first <- users[1:rl$remaining]
+      users_rest <- users[-(1:rl$remaining)]
+      print(glue("Getting data for {length(users_first)} users"))
+      tweets_first <- get_timeline(users_first, n, check = FALSE)
+      rl <- rate_limit(query = "get_timeline")
+    }else{
+      tweets_first <- NULL
+      users_rest <- users
+    }
+    wait <- rl$reset + 0.1
+    print(glue("Waiting for {round(wait,2)} minutes"))
+    Sys.sleep(wait * 60)
+    
+    tweets_rest <- get_timeline_unlimited(users_rest, n)  
+    tweets <- bind_rows(tweets_first, tweets_rest)
+  }
+  return(tweets)
+}
+
+#collect 100 past tweets from full list of candidates
+#candidate_timelines <- get_timeline_unlimited(handles_only_char_list, 100)
+
+candidate_timelines_select <- candidate_timelines %>% 
+  select(user_id:text, hashtags, media_url, media_type) %>% 
   mutate(screen_name = tolower(screen_name)) 
 
 #join to get the political party
+
 candidate_tweets <- 
-  handles_only %>% 
-  left_join(candidate_timelines, by = c("screen_name" = "twiter_handle"))
+candidate_timelines_select %>% 
+  left_join(handles_only, by = c("screen_name" = "twitter_handle"))
+
+saveRDS(candidate_timelines, file = "./App-1/full_candidate_tl.rds")
+saveRDS(candidate_timelines_select, file = "./App-1/short_candidate_tl.rds")
+
+saveRDS(candidate_timelines, file = "./App-1/short_candidate_tl_joined.rds")
 
 
 
